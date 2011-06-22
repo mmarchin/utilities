9595afcaa51d60e33b6e93c603c871a2d750fb11
Madelaine Gogol <madelaine@gmail.com>
2011-06-22 09:10:45 -0500

    modified checkservers.sh

--- checkservers.sh
+++ checkservers.sh
@@ -2,10 +2,8 @@
 
 #from stackoverflow.com/questions/1911144
 
-out=avail.txt
-rm -rf $out
 minLoad=1
-for h in $(cat listofservers.txt); do
+for h in $(cat ~/listofservers.txt); do
 	load=`ssh mcm@$h uptime`
 	echo "$h $load";
 done
